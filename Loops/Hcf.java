4cca33d0f4dcae9550f0b639614da0126afd9e5d
vishakha <vishakhakasture302002@gmail.com>
2024-03-31 19:33:53 +0530

    initial commit

--- Loops/Hcf.java
+++ Loops/Hcf.java
@@ -1,5 +1,25 @@
 package Loops;
 
-public class Hcf {
+import java.util.Scanner;
 
+public class Hcf {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter two numbers to find HCF of them.");
+    int n1 = sc.nextInt();
+    int n2 = sc.nextInt();
+    int small;
+    int hcf = 1;
+    if (n1 < n2) {
+      small = n1;
+    } else {
+      small = n2;
+    }
+    for (int i = 1; i <= small; i++) {
+      if (n1 % i == 0 && n2 % i == 0) {
+        hcf = i;
+      }
+    }
+    System.out.println("HCF is: " + hcf);
+  }
 }
--- Loops/Lcm.java
+++ Loops/Lcm.java
@@ -1,5 +1,24 @@
 package Loops;
 
-public class Lcm {
+import java.util.Scanner;;
 
+public class Lcm {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter two numbers to find LCM of them.");
+    int n1 = sc.nextInt();
+    int n2 = sc.nextInt();
+    int great;
+    if (n1 > n2) {
+      great = n1;
+    } else {
+      great = n2;
+    }
+    for (int i = great; i <= n1 * n2; i++) {
+      if (i % n1 == 0 && i % n2 == 0) {
+        System.out.println("LCM is: " + i);
+        break;
+      }
+    }
+  }
 }
