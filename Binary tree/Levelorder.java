2d0a2f51302183d3308294705784fadb3643628c
vishakha <vishakhakasture302002@gmail.com>
2024-10-11 22:33:16 +0530

    Btree

--- Binary tree/Levelorder.java
+++ Binary tree/Levelorder.java
@@ -1,7 +1,10 @@
-public class Postorder {
+import java.util.*;
+
+public class Levelorder {
   static class Node {
     int data;
-    Node left, right;
+    Node left;
+    Node right;
 
     Node(int data) {
       this.data = data;
@@ -18,7 +21,6 @@ public class Postorder {
       if (nodes[idx] == -1) {
         return null;
       }
-
       Node newNode = new Node(nodes[idx]);
       newNode.left = buildTree(nodes);
       newNode.right = buildTree(nodes);
@@ -26,24 +28,40 @@ public class Postorder {
       return newNode;
     }
 
-    public static void postorder(Node root) {
+    public static void levelOrder(Node root) {
       if (root == null) {
         return;
       }
 
-      postorder(root.left);
-      postorder(root.right);
-      System.out.print(root.data + "  ");
+      Queue<Node> q = new LinkedList<>();
+      q.add(root);
+      q.add(null);
+      while (!q.isEmpty()) {
+        Node currNode = q.remove();
+        if (currNode == null) {
+          System.out.println();
+          if (q.isEmpty()) {
+            break;
+          } else {
+            q.add(null);
+          }
+        } else {
+          System.out.print(currNode.data + " ");
+          if (currNode.left != null) {
+            q.add(currNode.left);
+          }
+          if (currNode.right != null) {
+            q.add(currNode.right);
+          }
+        }
+      }
     }
-
   }
 
   public static void main(String[] args) {
     int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
     BinaryTree tree = new BinaryTree();
     Node root = tree.buildTree(nodes);
-
-    tree.postorder(root);
-
+    tree.levelOrder(root);
   }
 }
